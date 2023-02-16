79ce4e039d262f626c5b37a84b77551d99d6772e
Raftouch <rafanelkin@gmail.com>
2023-02-16 18:40:34 +0100

    :tada: initial commit

---
+++ src/components/Product.tsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react'
+import { MyProduct } from '../models'
+
+interface ProductProps {
+    product: MyProduct
+}
+// параметр product типа MyProduct
+
+export function Product({ product }: ProductProps) {
+    const [details, setDetails] = useState(false)
+    const btnColor = details ? 'bg-red-400' : 'bg-green-400'
+    const btnColors = ['py-2 px-4 border rounded', btnColor]
+
+    return (
+        <div 
+            className='border py-2 px-4 rounded flex flex-col items-center mb-2'
+            >
+            <img src={ product.image } className='w-1/6' alt={product.title} />
+            <p>{ product.title }</p>
+            <p className='font-bold'>{product.price}</p>
+
+            <button 
+            className={btnColors.join(' ')}
+            onClick={() => setDetails(prev => !prev)}
+            >
+                { details ? 'Hide details' : 'Show details' }
+            </button>
+
+            {details && <div>
+                <p>{ product.description }</p>
+                <p>Rate: <span style={{ fontWeight: 'bold' }}>{ product.rating.rate }</span></p>
+            </div>}
+        </div>
+    )
+}
